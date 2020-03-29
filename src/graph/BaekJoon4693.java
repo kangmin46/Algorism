8ff868083a749c69bea9c5e4304de9dc025c36ae
kangmin46 <kangmin789@naver.com>
2020-03-29 14:42:00 +0900

    update 4693

--- src/graph/BaekJoon4693.java
+++ src/graph/BaekJoon4693.java
@@ -5,46 +5,49 @@ import java.util.Scanner;
 public class BaekJoon4693 {
     static int[][] map;
     static int[][] temp;
-    static int w;
-    static int h;
-    static int[] global_dh = {0, 0, 1, -1, 1, 1, -1, -1};
-    static int[] global_dw = {1, -1, 0, 0, 1, -1, 1, -1};
-
+    static int[] dx = {1, -1, 0, 0,-1,1,1,-1};
+    static int[] dy = {0, 0, 1, -1,1,-1,1,-1};
+    static int globalX;
+    static int globalY;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        w = scanner.nextInt();
-        h = scanner.nextInt();
-        map = new int[w][h];
-        temp = new int[w][h];
 
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                map[j][i] = scanner.nextInt();
-            }
-        }
+        while (true) {
+            globalX = scanner.nextInt();
+            globalY = scanner.nextInt();
+            if (globalX == 0 && globalY == 0) break;
 
-        int count = 0;
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if (map[j][i] == 1 && temp[j][i] == 0) {
-                    dfs(j, i, ++count);
+            map = new int[globalY][globalX];
+            temp = new int[globalY][globalX];
+
+            for (int i = 0; i < globalY; i++) {
+                for (int j = 0; j < globalX; j++) {
+                    map[i][j] = scanner.nextInt();
                 }
             }
-        }
 
-        System.out.println(count);
+            int count = 0;
+            for (int i = 0; i < globalY; i++) {
+                for (int j = 0; j < globalX; j++) {
+                    if (map[i][j] == 1 && temp[i][j] == 0) {
+                        bfs(i, j, ++count);
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
     }
 
-    private static void dfs(int localW, int localH, int count) {
-        temp[localW][localH] = count;
+    private static void bfs(int y, int x, int count) {
+        temp[y][x] = count;
 
         for (int i = 0; i < 8; i++) {
-            int dh = global_dh[i] + localH;
-            int dw = global_dw[i] + localW;
-            if (dh >= 0 && dh < h && dw >= 0 && dw < w) {
-                if (temp[dw][dh] == 0 && map[dw][dh] == 1) {
-                    dfs(dw, dh, count);
-                }
+            int localY = dy[i] + y;
+            int localX = dx[i] + x;
+
+            if(localX >= 0 && localX < globalX && localY >= 0 && localY < globalY) {
+                if(map[localY][localX] == 1 && temp[localY][localX] == 0) bfs(localY, localX, count);
             }
         }
     }
